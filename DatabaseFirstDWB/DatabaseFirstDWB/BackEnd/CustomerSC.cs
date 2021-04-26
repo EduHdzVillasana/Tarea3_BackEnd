1bbe304aa7df2a71b9a566ce5cf3332ea4850763
Eduardo Alan Hernández Villasana <eduardo.hernandezvll@uanl.edu.mx>
2021-04-25 20:05:51 -0500

    Corrección

---
+++ DatabaseFirstDWB/DatabaseFirstDWB/BackEnd/CustomerSC.cs
@@ -0,0 +1,58 @@
+﻿using DatabaseFirstDWB.DataAccess;
+using DatabaseFirstDWB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDWB.BackEnd
+{
+    public class CustomerSC : BaseSC
+    {
+        public IQueryable<Customer> GetAllCustomers ()
+        {
+            return dbContext.Customers.Select(s => s);
+        }
+
+        public Customer GetCustomerById (string id)
+        {
+            return GetAllCustomers().Where(w => w.CustomerId == id).FirstOrDefault();
+        }
+
+        public void RemoveCustomer (string id)
+        {
+            var removedCustomer = GetCustomerById(id);
+            dbContext.Customers.Remove(removedCustomer);
+            dbContext.SaveChanges();
+        }
+
+        public void UpdateCustomer (string id, CustomerModel customer)
+        {
+            Customer currentCustomer = GetCustomerById(id);
+            if (currentCustomer == null)
+            {
+                throw new Exception("No se encontró el cliente con el ID proporcionado");
+            }
+            currentCustomer.CompanyName = customer.name;
+            currentCustomer.City = customer.cityName;
+            currentCustomer.Phone = customer.phoneNumber;
+
+            dbContext.SaveChanges();
+        }
+
+        public void AddCustomer (CustomerModel newCustomer)
+        {
+            var newCustomerRegister = new Customer()
+            {
+                CustomerId = newCustomer.idString,
+                CompanyName = newCustomer.name,
+                City = newCustomer.cityName,
+                Phone = newCustomer.phoneNumber
+            };
+
+            dbContext.Customers.Add(newCustomerRegister);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ DatabaseFirstDWB/DatabaseFirstDWB/BackEnd/EmployeeSC.cs
@@ -0,0 +1,67 @@
+﻿using DatabaseFirstDWB.DataAccess;
+using DatabaseFirstDWB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDWB.BackEnd
+{
+    public class EmployeeSC : BaseSC
+    {
+
+        public IQueryable<Employee> GetAllEmployees()
+        {
+            return dbContext.Employees.Select(s => s);
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            return GetAllEmployees().Where(w => w.EmployeeId == id).FirstOrDefault();
+        }
+        public void FireEmployee(int id)
+        {
+            var firedEmployee = GetEmployeeById(id);
+            //Console.WriteLine(firedEmployee.FirstName);
+            dbContext.Employees.Remove(firedEmployee);
+            dbContext.SaveChanges();
+        }
+        public void UpdateEmployeeFirstNameById(int id, string name)
+        {
+            Employee currentEmployee = GetEmployeeById(id);
+            if (currentEmployee == null)
+            {
+                throw new Exception("No se encontro el empleado con el ID proporcionado");
+            }
+
+            currentEmployee.FirstName = name;
+            dbContext.SaveChanges();
+        }
+
+        public void UpdateEmployee(int id, EmployeeModel employee)
+        {
+            Employee currentEmplpyee = GetEmployeeById(id);
+            if (currentEmplpyee == null)
+            {
+                throw new Exception("No se encontró el empleado con el ID proporcionado");
+            }
+            currentEmplpyee.FirstName = employee.Name;
+            currentEmplpyee.LastName = employee.FamilyName;
+            dbContext.SaveChanges();
+        }
+
+        public void AddEmployee(EmployeeModel newEmployee)
+        {
+            var newEmployeeRegister = new Employee()
+            {
+                FirstName = newEmployee.Name,
+                LastName = newEmployee.FamilyName
+            };
+
+            dbContext.Employees.Add(newEmployeeRegister);
+            dbContext.SaveChanges();
+
+        }
+
+    }
+}
